dde09712f5dbc875472c764dc72cda86c299816c
mtakbaba <takbaba@valven.com>
2026-03-18 11:53:20 +0300

    40 lines gitignore change and more than 100 lines py file changes

--- test.py
+++ test.py
@@ -32,3 +32,275 @@ after commit
 
 
 asdasd
+def main():
+  smile = true
+  if (smile):
+    pr
+asdasdas
+asd
+third commit 
+a
+ds
+
+
+
+first commit 
+
+
+
+second commit 
+
+new commit
+
+
+another commit 
+
+
+asdasdss
+
+
+
+
+
+after commit
+
+
+asdasd
+def main():
+  smile = true
+  if (smile):
+    pr
+asdasdas
+asd
+third commit 
+a
+ds
+
+
+
+first commit 
+
+
+
+second commit 
+
+new commit
+
+
+another commit 
+
+
+asdasdss
+
+
+
+
+
+after commit
+
+
+asdasd
+def main():
+  smile = true
+  if (smile):
+    pr
+asdasdas
+asd
+third commit 
+a
+ds
+
+
+
+first commit 
+
+
+
+second commit 
+
+new commit
+
+
+another commit 
+
+
+asdasdss
+
+
+
+
+
+after commit
+
+
+asdasd
+def main():
+  smile = true
+  if (smile):
+    pr
+asdasdas
+asd
+third commit 
+a
+ds
+
+
+
+first commit 
+
+
+
+second commit 
+
+new commit
+
+
+another commit 
+
+
+asdasdss
+
+
+
+
+
+after commit
+
+
+asdasd
+def main():
+  smile = true
+  if (smile):
+    pr
+asdasdas
+asd
+third commit 
+a
+ds
+
+
+
+first commit 
+
+
+
+second commit 
+
+new commit
+
+
+another commit 
+
+
+asdasdss
+
+
+
+
+
+after commit
+
+
+asdasd
+def main():
+  smile = true
+  if (smile):
+    pr
+asdasdas
+asd
+third commit 
+a
+ds
+
+
+
+first commit 
+
+
+
+second commit 
+
+new commit
+
+
+another commit 
+
+
+asdasdss
+
+
+
+
+
+after commit
+
+
+asdasd
+def main():
+  smile = true
+  if (smile):
+    pr
+asdasdas
+asd
+third commit 
+a
+ds
+
+
+
+first commit 
+
+
+
+second commit 
+
+new commit
+
+
+another commit 
+
+
+asdasdss
+
+
+
+
+
+after commit
+
+
+asdasd
+def main():
+  smile = true
+  if (smile):
+    pr
+asdasdas
+asd
+third commit 
+a
+ds
+
+
+
+first commit 
+
+
+
+second commit 
+
+new commit
+
+
+another commit 
+
+
+asdasdss
+
+
+
+
+
+after commit
+
+
+asdasd
